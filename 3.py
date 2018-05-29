395c9af6191b75d1f313e54d073ec91669bff439
Deb Roy <iamdebkrishnaroy@gmail.com>
2018-05-30 01:10:19 +0530

    Just a Sample Python file

---
+++ 3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  9 17:11:28 2018
+
+@author: guddu
+"""
+
+num=input("Enter Number : ")
+if (num<0):
+    print "Please Enter a Valid Number"
+else:
+    print "Square root : ", num**0.5
+    
